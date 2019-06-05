de0410ca576273677779bc65fb756ce6a299d31d
Shen Suhao <vagrant@homestead>
2019-06-05 02:42:34 +0000

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,7 +7,13 @@ use App\Http\Requests;
 use Auth;
 
 class SessionsController extends Controller
-{
+{	
+	public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     public function create()
     {
     	return view('session.create');
@@ -20,7 +26,7 @@ class SessionsController extends Controller
            'password' => 'required'
        ]);
 
-       if (Auth::attempt($credentials)) {
+       if (Auth::attempt($credentials,$request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
            return redirect()->route('users.show', [Auth::user()]);
        } else {
@@ -28,7 +34,7 @@ class SessionsController extends Controller
            return redirect()->back();
        }
     }
-    
+
     public function destroy()
     {
         Auth::logout();
